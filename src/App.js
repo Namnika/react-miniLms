cb32434dd0e44893bc4fabf253f1d05cef85f02d
Namnika <janbandhunamnika@gmail.com>
2025-07-10 12:59:01 +0530

    add all files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React mini lms</h1>
-      </header>
+      
     </div>
   );
 }
