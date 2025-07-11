ac42465fd20e956c35bafcb0e7470d87055789c4
Namnika <janbandhunamnika@gmail.com>
2025-07-11 16:08:06 +0530

    update login input styles

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -3,10 +3,12 @@ import LoginImg from "../../assests/login-portal-img.jpg";
 
 const Login = () => {
 
-    const [showPassword, setShowPassword ] = useState(false)
+  const [showPassword, setShowPassword] = useState(false)
+
+
   return (
     <div className="grid min-h-screen grid-cols-2 h-full w-full">
-      <div className="login-wrapper bg-gray-950 py-5 text-white">
+      <div className="login-wrapper flex flex-col justify-center px-7 bg-gray-950 py-5 text-white">
         <div className=" m-15">
           <div className="login-heading py-3">
             <h2 className="font-bold text-5xl">Login</h2>
@@ -15,7 +17,7 @@ const Login = () => {
           </div>
           <div className="login-inputs my-6 gap-3 flex flex-col">
             <input
-              className="placeholder:text-gray-500 p-3 border-b"
+              className="placeholder:text-gray-500 rounded p-3 border-b"
               type="email"
               name="email"
               required
@@ -23,22 +25,24 @@ const Login = () => {
             />
             <div className="relative w-full">
               <input
-                className="placeholder:text-gray-500 p-3 border-b"
-                type="password"
+                className="rounded placeholder:text-gray-500 w-full p-3 border-b"
+                type={showPassword ? "text" : "password"}
                 name="password"
                 required
                 placeholder="Password"
               />
-              <span 
-              className="absolute r-5 t-[50%] translate-y-[50%] pointer"
-                
+              <span
+                className="absolute right-7
+    top-3 "
+                onClick={() => setShowPassword(!showPassword)}
+
               >
-                {showPassword ? "show eye icon" : "hide eye icon"}
+                {showPassword ? <i className="fa-solid fa-eye "></i> : <i className="fa-solid fa-eye-slash "></i>}
               </span>
             </div>
             <div>
               <button
-                className="w-full bg-violet-500 h-8 my-3 rounded"
+                className="w-full bg-violet-500 hover:bg-violet-400 h-8 my-3 rounded"
                 type="submit"
               >
                 Login
@@ -47,15 +51,15 @@ const Login = () => {
           </div>
           <div className="no-login flex gap-5">
             <h4 className="text-gray-400">Don't have an account?</h4>
-            <button className="bg-gray-700 font-semibold text-gray-300 h-8 w-19 rounded">
+            <button className="bg-gray-700 hover:bg-gray-500 font-semibold text-gray-300 h-8 w-19 rounded">
               Sign up
             </button>
           </div>
         </div>
       </div>
-      <div className="portal w-full h-full bg-white right-column bg-white">
+      <div className="portal w-full h-full flex right-column ">
         <div className="portal-heading">
-          <img src={LoginImg} className=" w-full h-full object-cover " />
+          <img src={LoginImg} className=" w-full h-full " alt="portal-image" />
         </div>
       </div>
     </div>
