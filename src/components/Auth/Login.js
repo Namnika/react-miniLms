b5ce85f500133290ad2f783c894a58de5afef96d
Namnika <janbandhunamnika@gmail.com>
2025-07-11 12:51:05 +0530

    add login password styles

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -8,7 +8,7 @@ const Login = () => {
         <div className=" m-15">
           <div className="login-heading py-3">
             <h2 className="font-bold text-5xl">Login</h2>
-            <br/>
+            <br />
             <h4 className="text-gray-400">Enter your account details</h4>
           </div>
           <div className="login-inputs my-6 gap-3 flex flex-col">
@@ -19,20 +19,35 @@ const Login = () => {
               required
               placeholder="Email"
             />
-            <input
-            className="placeholder:text-gray-500 p-3 border-b"
-              type="password"
-              name="password"
-              required
-              placeholder="Password"
-            />
+            <div className="relative w-full">
+              <input
+                className="placeholder:text-gray-500 p-3 border-b"
+                type="password"
+                name="password"
+                required
+                placeholder="Password"
+              />
+              <span 
+              className="absolute r-5 t-[50%] translate-y-[50%] pointer"
+                
+              >
+                {showPassword ? "show eye icon" : "hide eye icon"}
+              </span>
+            </div>
             <div>
-              <button className="w-full bg-violet-500 h-8 my-3 rounded" type="submit">Login</button>
+              <button
+                className="w-full bg-violet-500 h-8 my-3 rounded"
+                type="submit"
+              >
+                Login
+              </button>
             </div>
           </div>
           <div className="no-login flex gap-5">
             <h4 className="text-gray-400">Don't have an account?</h4>
-            <button className="bg-gray-700 font-semibold text-gray-300 h-8 w-19 rounded">Sign up</button>
+            <button className="bg-gray-700 font-semibold text-gray-300 h-8 w-19 rounded">
+              Sign up
+            </button>
           </div>
         </div>
       </div>
