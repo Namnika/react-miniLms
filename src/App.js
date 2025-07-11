6be6d838020a7788681079ab0dec290e9dc7b281
Namnika <janbandhunamnika@gmail.com>
2025-07-11 12:41:06 +0530

    add admin dashboard styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Auth/Login';
+import AdminDashboard from './components/Admin/AdminDashboard';
 import './index.css';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/users' element={<Login />}/>
+        <Route path='/' element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
