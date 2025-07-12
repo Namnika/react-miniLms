88495096fdc5b0b7d5f93eb75e08d0697f80d569
Namnika <janbandhunamnika@gmail.com>
2025-07-12 16:46:05 +0530

    modify cors oprtions in server

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,43 @@
-// index.js
+require("dotenv").config({ quiet: true });
+const cors = require("cors");
 const express = require('express');
+const userRoutes = require('./routes/userRoutes')
+const credentials = require("./middleware/authMiddleware");
+const corsOptions = require("./config/corsOptions");
+const mongoose = require("mongoose");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-// Middleware to parse JSON
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencode form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
 app.use(express.json());
 
-// A basic route
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+
+// logout user and clear its token
+app.get("/logout", (req, res) => {
+  req.logout(err => {
+    if (err) return res.status(500).send('Logout failed');
+    res.clearCookie('connect.sid'); // Clear session cookie
+    res.status(200).json({ message: "Logged out successfully." });
+    res.redirect('/users');
+  });
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// routes
+app.use("/", userRoutes);
+
+
+// connecting node with mongodb
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => { console.log('Connected to MongoDB'), app.listen(PORT, () => console.log(`Server is running on port: ${PORT}!`)); })
+  .catch(err => console.error('MongoDB connection error:', err));
