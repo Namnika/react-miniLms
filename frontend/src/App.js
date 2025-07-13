8cf93c6d7297cef7106dbde7cd03edaa5885a427
Namnika <janbandhunamnika@gmail.com>
2025-07-13 17:53:20 +0530

    add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,44 @@ import Login from './components/Auth/Login';
 import AdminDashboard from './components/Admin/AdminDashboard';
 import './index.css';
 import Signup from './components/Auth/Signup';
+
+
+// const PrivateRoute = ({ children, role }) => {
+//   const token = localStorage.getItem('token');
+//   const user = JSON.parse(localStorage.getItem('user'));
+
+//   if (!token || !user) return <Navigate to="/login" />;
+
+//   // Optionally check role
+//   if (role && user.role !== role) {
+//     return <Navigate to="/unauthorized" />;
+//   }
+
+//   return children;
+// };
+
+// export default PrivateRoute;
+// eslint-disable-next-line no-lone-blocks
+{/* <Route
+  path="/admin/dashboard"
+  element={
+    <PrivateRoute role="admin">
+      <AdminDashboard />
+    </PrivateRoute>
+  }
+/> */}
+
+
+
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/users/params' element={<Login />}/>
-        <Route path='/users' element={<Signup />}/>
-        <Route path='/' element={<AdminDashboard />} />
+      <Route path='/users' element={<Signup />}/>
+      <Route path='/login/users' element={<Login />}/>
+        
+        <Route path='/student/dashboard' element={<AdminDashboard />} />
       </Routes>
     </Router>
   );
