4e2d3088689b1437b69f84250f0c1815a7b93286
Namnika <janbandhunamnika@gmail.com>
2025-07-13 17:55:00 +0530

    add routers for dashboards

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router();
 const {signup, login} = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/users', signup)
-router.get('/users', login)
+router.post('/login/users', login)
+
+// authentication token for student dashboard
+router.get('/student/dashboard', authMiddleware, (req, res) => {
+    res.json({ user: req.user });
+});
+router.get('/admin/dashboard', authMiddleware, (req, res) => {
+    res.json({ user: req.user });
+});
+
 
 module.exports = router
