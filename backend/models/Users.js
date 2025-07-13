8779ac325f5bf4f1308e87646feef3a5bff8dc83
Namnika <janbandhunamnika@gmail.com>
2025-07-13 16:39:24 +0530

    add 'required: true' for name

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -6,7 +6,8 @@ const userSchema = new mongoose.Schema({
         type: String
     },
     name: {
-        type: String
+        type: String,
+        required: true
     },
     password: {
         type: String
