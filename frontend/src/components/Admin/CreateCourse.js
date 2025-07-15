f900461de996e37188f1ed57bcb138a49bfb26b6
Namnika <janbandhunamnika@gmail.com>
2025-07-15 12:19:54 +0530

    add create course component to add course content

---
+++ frontend/src/components/Admin/CreateCourse.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+
+function CreateCourse() {
+  const [value, setValue] = useState("");
+  return (
+    <div>
+      <div id="course-edit" className="my-8">
+        <div className="flex mb-7 flex-row ">
+          <label
+            for="small-input"
+            className=" mb-2 w-xs  text-gray-900 dark:text-white"
+          >
+            <strong>Course Name:</strong>
+          </label>
+          <input
+            type="text"
+            id="small-input"
+            className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        {/* react quill editor */}
+        <div>
+          <ReactQuill
+            className="bg-gray-50"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+          {/* Optional: Display the raw HTML output */}
+          <div style={{ marginTop: "20px" }}>
+            <strong>Course Content:</strong>
+            <div dangerouslySetInnerHTML={{ __html: value }} />
+          </div>
+        </div>
+      </div>
+      <div id="all-courses" className="course-button">
+        <button className="w-full bg-violet-500 hover:bg-violet-400 h-8 my-3 rounded text-white font-semibold uppercase">
+          Create Course
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CreateCourse;
