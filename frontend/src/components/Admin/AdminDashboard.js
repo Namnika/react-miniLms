98de18a0ca490fa3d8b477fe8dda0b415669cb8a
Namnika <janbandhunamnika@gmail.com>
2025-07-16 19:55:48 +0530

    removed styles for dark theme

--- frontend/src/components/Admin/AdminDashboard.js
+++ frontend/src/components/Admin/AdminDashboard.js
@@ -23,10 +23,10 @@ const AdminDashboard = () => {
               <li>
                 <a
                   href="#course-edit"
-                  className="flex items-center p-2 text-white rounded-lg dark:text-white hover:text-violet-300 dark:hover:bg-gray-700 group"
+                  className="flex items-center p-2 text-white rounded-lg hover:text-violet-300  group"
                 >
                   <svg
-                    className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    className="w-5 h-5 text-gray-500 transition duration-75  group-hover:text-gray-900 "
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -67,13 +67,13 @@ const AdminDashboard = () => {
         {/* View all courses */}
         <div>
           <h2 className="text-xl font-bold my-5"> View All Courses</h2>
-          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm ">
             <a href="#">
               <img className="rounded-t-lg" src={CourseImg} alt="" />
             </a>
             <div className="p-5">
               <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                   Noteworthy technology acquisitions 2021
                 </h5>
               </a>
--- frontend/src/components/Admin/CreateCourse.js
+++ frontend/src/components/Admin/CreateCourse.js
@@ -10,14 +10,14 @@ function CreateCourse({value, setValue}) {
         <div className="flex mb-7 flex-row ">
           <label
             for="small-input"
-            className=" mb-2 w-xs  text-gray-900 dark:text-white"
+            className=" mb-2 w-xs  text-gray-900"
           >
             <strong>Course Name:</strong>
           </label>
           <input
             type="text"
             id="small-input"
-            className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 "
           />
         </div>
         {/* react quill editor */}
