b125649f9c6b1a883abd1e470dadbbfd69f12476
Namnika <janbandhunamnika@gmail.com>
2025-07-17 00:23:35 +0530

    modify html content using striptags

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -3,12 +3,13 @@ const Courses = require('../models/Courses')
 exports.courseCreate = async (req, res) => {
 	try {
 		const { title, content } = req.body
+		const plainText = striptags(content);
 
 		if (!title || !content) {
 			return res.status(400).json({ message: "Title and content are required!" })
 		}
 
-		const newCourse = new Courses({ title, content })
+		const newCourse = new Courses({ title, content, plainText })
 		await newCourse.save()
 
 		res.status(201).json({ message: "Course created successfully!" })
