6a92b19b6a8c7b02b956c8ba589a6e39720b792d
Namnika <janbandhunamnika@gmail.com>
2025-07-16 19:58:18 +0530

    modify styles

--- frontend/src/components/Admin/AdminDashboard.js
+++ frontend/src/components/Admin/AdminDashboard.js
@@ -42,10 +42,10 @@ const AdminDashboard = () => {
               <li>
                 <a
                   href="#all-courses"
-                  className="flex items-center p-2  rounded-lg dark:text-white hover:text-violet-300 dark:hover:bg-gray-700 group"
+                  className="flex items-center p-2  rounded-lg dark:text-white hover:text-violet-300  group"
                 >
                   <svg
-                    className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 "
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
