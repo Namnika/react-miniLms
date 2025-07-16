b9d6763a8eb0aa05bc9fa3fd794a0ac21ad4a0a6
Namnika <janbandhunamnika@gmail.com>
2025-07-16 12:56:27 +0530

    add course view styles

--- frontend/src/components/Student/CourseView.js
+++ frontend/src/components/Student/CourseView.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function CourseView() {
   return (
@@ -6,7 +7,7 @@ function CourseView() {
       <div className="w-xl pl-18 py-12 m-auto">
         <div class=" fixed w-full top-0 start-0 border-b border-gray-200">
           <header className="bg-gray-950 text-white">
-            <div class="max-w-screen-xl flex flex-row  items-center justify-center mx-auto p-15">
+            <div class="max-w-screen-xl flex flex-row  items-center justify-center mx-8 p-15">
               <a href="#">
                 <span class="self-center text-base font-semibold whitespace-nowrap dark:text-white">
                   Course View
@@ -20,12 +21,23 @@ function CourseView() {
                   Noteworthy technology acquisitions 2021
                 </h2>
               </div>
+              <a href="#"><Link to={"/student/dashboard"}>
+                <span class="self-center text-base font-semibold whitespace-nowrap dark:text-white">
+                  All Courses
+                </span></Link>
+              </a>
             </div>
           </header>
           <p className="m-auto py-10 w-xl font-normal text-gray-700 dark:text-gray-400">
             Here are the biggest enterprise technology acquisitions of 2021 so
             far, in reverse chronological order.
           </p>
+
+          <div id="all-courses" className="course-button text-center">
+            <button className="w-sm bg-violet-500  hover:bg-violet-400 h-8 my-3 rounded text-white font-semibold uppercase">
+              Mark As Completed
+            </button>
+          </div>
         </div>
       </div>
     </div>
