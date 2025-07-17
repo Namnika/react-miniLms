7d61ffe4e34cc450c001afcd912dc8db7cec12d8
Namnika <janbandhunamnika@gmail.com>
2025-07-17 12:46:03 +0530

    add course view

--- frontend/src/components/Student/CourseView.js
+++ frontend/src/components/Student/CourseView.js
@@ -1,52 +1,85 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 function CourseView() {
-
-  const {state} = useLocation()
-  const course = state?.course
+  const { state } = useLocation();
+  const course = state?.course;
+  const setProgress = state?.setProgress;
 
   if (!course) return <p>Course not found</p>; // fallback
 
-  return (
-    <div className="bg-violet-100/50 h-screen">
-      <div className="w-xl pl-18 py-12 m-auto">
-        <div class=" fixed w-full top-0 start-0 ">
-          <header className="bg-gray-950 text-white">
-            <div class="max-w-screen-xl flex flex-row  items-center justify-center mx-8 p-15">
-              <a href={`${course.id}`} onClick={(e) => e.preventDefault()} className="cursor-pointer">
-                <span class="self-center text-base font-semibold whitespace-nowrap ">
-                  Course View
-                </span>
-              </a>
-              <div
-                class="items-center text-center px-8 justify-center w-full md:flex "
-                id="navbar-sticky"
-              >
-                <h2 className="text-2xl font-semibold ">
-                  {course.title}
-                </h2>
-              </div>
-              <a href="#"><Link to={"/student/dashboard"}>
-                <span class="self-center text-base font-semibold whitespace-nowrap ">
-                  All Courses
-                </span></Link>
-              </a>
-            </div>
-          </header>
-          <p className="m-auto p-10 w-xl font-normal text-left text-gray-700 ">
-            {course.content}
-          </p>
+  const userId = "68725d72247c1d34534c2251";
 
-          <div id="all-courses" className="course-button text-center">
-            <button className="w-sm bg-violet-500  hover:bg-violet-400 h-8 my-3 rounded text-white font-semibold uppercase">
-              Mark As Completed
-            </button>
+  const handleComplete = async () => {
+    try {
+      // 1. Send POST to backend
+      await axios.post("http://localhost:5000/progress/mark-completed", {
+        userId,
+        courseId: course._id,
+      });
+
+      // 2. Update progress in UI
+      if (setProgress === 50) setProgress(100);
+
+      // Optional: show toast or success message
+      setTimeout(() => {
+        console.log("Course marked as completed");
+        toast.success("Course marked as completed");
+      }, 2000);
+    } catch (error) {
+      console.error("Error marking course as completed", error);
+      toast.error(`Error marking course as completed ${error}`);
+    }
+  };
+
+  return (
+    <><div><Toaster position="top-right"/></div>
+      <div className="bg-violet-100/50 h-screen">
+        <div className="w-xl pl-18 py-12 m-auto">
+          <div class=" fixed w-full top-0 start-0 ">
+            <header className="bg-gray-950 text-white">
+              <div class="max-w-screen-xl flex flex-row  items-center justify-center mx-8 p-15">
+                <a
+                  href={`${course.id}`}
+                  onClick={(e) => e.preventDefault()}
+                  className="cursor-pointer"
+                >
+                  <span class="self-center text-base font-semibold whitespace-nowrap ">
+                    Course View
+                  </span>
+                </a>
+                <div
+                  class="items-center text-center px-8 justify-center w-full md:flex "
+                  id="navbar-sticky"
+                >
+                  <h2 className="text-2xl font-semibold ">{course.title}</h2>
+                </div>
+                <Link to={"/student/dashboard"}>
+                  <span class="self-center text-base font-semibold whitespace-nowrap ">
+                    All Courses
+                  </span>
+                </Link>
+              </div>
+            </header>
+            <p className="m-auto p-10 w-xl font-normal text-left text-gray-700 ">
+              {course.content}
+            </p>
+
+            <div id="all-courses" className="course-button text-center">
+              <button
+                onClick={handleComplete}
+                className="w-sm bg-violet-500  hover:bg-violet-400 h-8 my-3 rounded text-white font-semibold uppercase"
+              >
+                Mark As Completed
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- frontend/src/components/Student/StudentDashboard.js
+++ frontend/src/components/Student/StudentDashboard.js
@@ -2,39 +2,71 @@ import React, { useState, useEffect } from "react";
 import CourseImg from "../../assests/course-image.jpg";
 import { Link } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
 
 function StudentDashboard() {
   const [progress, setProgress] = useState(50);
   const [isCompleted, setIsCompleted] = useState(false);
   const [view, setView] = useState("all"); // "all" or "completed"
 
-  useEffect(() => {
-    if (progress === 100) {
-      setIsCompleted(true);
-    } else {
-      setProgress(() => progress + 50);
-      setIsCompleted(false);
+  //   const userId = "68725d72247c1d34534c2251"; // or get from context/auth
+  // const courseId = course._id; // from props or location.state
+
+  const getProgress = async () => {
+    try {
+      const res = axios.get("http://localhost:5000/progress/:userId/:courseId"); // changing later
+      if (res?.status === "Completed") {
+        setProgress(100);
+        setIsCompleted(true);
+      } else {
+        setProgress(50); // or whatever logic fits your app
+        setIsCompleted(false);
+      }
+    } catch (error) {
+      console.error("Error in getting progress bar", error);
     }
-  }, [progress]);
+  };
 
   const courses = [
-    { id: 1, title: "Noteworthy technology acquisitions 2021", content: "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.", status: "Completed", img: { CourseImg } },
-    { id: 2, title: "Vue Essentials", status: "Pending", content: "imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's ", img: { CourseImg } },
-    { id: 3, title: "Tailwind Mastery", content: "standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled", status: "Completed", img: { CourseImg } },
+    {
+      id: 1,
+      title: "Noteworthy technology acquisitions 2021",
+      content:
+        "Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.",
+      status: "Completed",
+      img: { CourseImg },
+    },
+    {
+      id: 2,
+      title: "Vue Essentials",
+      status: "Pending",
+      content:
+        "imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's ",
+      img: { CourseImg },
+    },
+    {
+      id: 3,
+      title: "Tailwind Mastery",
+      content:
+        "standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
+      status: "Completed",
+      img: { CourseImg },
+    },
   ];
 
   // Filter logic based on selected view
   const filteredCourses =
     view === "Completed"
-      ? courses.filter(course => course.status === "Completed") :
-      view === "Pending" ? courses.filter(course => course.status === "Pending")
-        : courses;
-
-
+      ? courses.filter((course) => course.status === "Completed")
+      : view === "Pending"
+      ? courses.filter((course) => course.status === "Pending")
+      : courses;
 
   return (
     <>
-      <div><Toaster position="top-right" /></div>
+      <div>
+        <Toaster position="top-right" />
+      </div>
       <div className="bg-violet-100/50">
         <div className="w-3xl pl-20 py-12 mx-auto">
           <h2 className="text-2xl font-bold text-violet-500">
@@ -50,7 +82,7 @@ function StudentDashboard() {
             <div className="h-full px-3 py-4 overflow-y-auto  bg-gray-950 ">
               <ul className="space-y-2 text-white  text-base font-medium">
                 <li>
-                  <button onClick={() => setView('all')}>
+                  <button onClick={() => setView("all")}>
                     <a
                       href="#all-courses"
                       className="flex items-center p-2  rounded-lg hover:text-violet-300 group"
@@ -71,7 +103,7 @@ function StudentDashboard() {
                   </button>
                 </li>
                 <li>
-                  <button onClick={() => setView('Completed')}>
+                  <button onClick={() => setView("Completed")}>
                     <a
                       href="#completed-course"
                       className="flex items-center p-2  rounded-lg  hover:text-violet-300"
@@ -92,7 +124,7 @@ function StudentDashboard() {
                   </button>
                 </li>
                 <li>
-                  <button onClick={() => setView('Pending')}>
+                  <button onClick={() => setView("Pending")}>
                     <a
                       href="#pending-course"
                       className="flex items-center p-2  rounded-lg  hover:text-violet-300"
@@ -116,77 +148,92 @@ function StudentDashboard() {
             </div>
           </aside>
 
-          <div >
+          <div>
             <h2 className="text-xl font-bold my-5"> Your All Courses</h2>
             <div className="grid grid-cols-2 grid-flow-row-dense gap-5">
-              {filteredCourses.map(course => {
-
-                return <div key={course.id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm ">
-                  <div className="relative">
-                    <span class={`inline-flex absolute z-10 top-2 right-2 items-center ${course.status === "Completed" ? "bg-green-100" : "bg-yellow-100"} ${course.status === "Completed" ? "text-green-800" : "text-yellow-800"} text-xs font-medium px-2.5 py-0.5 rounded-full`}>
-                      <span class={`w-2 h-2 me-1 ${course.status === "Completed" ? "bg-green-500" : "bg-yellow-500"} rounded-full`}></span>
-                      {course.status}
-                    </span>
-                    <a href="#">
-                      <img className="rounded-t-lg" src={CourseImg} alt="" />
-                    </a>
-                  </div>
-                  <div className="p-5">
-                    <a href="#all-courses">
-                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-                        {course.title}
-                      </h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 ">
-                      {course.content}
-                    </p>
-
-                    <div>
-                      <div
-                        class=" bg-gray-200 rounded-full "
-                        style={{ width: "100%" }}
+              {filteredCourses.map((course) => {
+                return (
+                  <div
+                    key={course.id}
+                    className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm "
+                  >
+                    <div className="relative">
+                      <span
+                        class={`inline-flex absolute z-10 top-2 right-2 items-center ${
+                          course.status === "Completed"
+                            ? "bg-green-100"
+                            : "bg-yellow-100"
+                        } ${
+                          course.status === "Completed"
+                            ? "text-green-800"
+                            : "text-yellow-800"
+                        } text-xs font-medium px-2.5 py-0.5 rounded-full`}
                       >
-                        {isCompleted && (
-                          <button
-                            disabled={!isCompleted}
-                            onClick={() => toast.success("Progress Completed!")}
-                          >
-                            <div
-                              class="bg-violet-600 text-xs font-medium text-violet-100 text-center p-0.5 leading-none rounded-full"
-                              style={{ width: `${progress}px` }}
-                            >
-                              {" "}
-                              {progress}%
-                            </div>
-                          </button>
-                        )}
-
-                      </div>
+                        <span
+                          class={`w-2 h-2 me-1 ${
+                            course.status === "Completed"
+                              ? "bg-green-500"
+                              : "bg-yellow-500"
+                          } rounded-full`}
+                        ></span>
+                        {course.status}
+                      </span>
+                      <a href="#">
+                        <img className="rounded-t-lg" src={CourseImg} alt="" />
+                      </a>
                     </div>
+                    <div className="p-5">
+                      <a href="#all-courses">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                          {course.title}
+                        </h5>
+                      </a>
+                      <p className="mb-3 font-normal text-gray-700 ">
+                        {course.content}
+                      </p>
 
-                    <button className="  border border-violet-500  hover:bg-violet-100 h-8 my-3 rounded text-violet-500 font-semibold">
-                      <Link to={`/student/courses/${course.id}`} state={{ course }} className="inline-flex items-center px-3 py-2 text-sm text-center font-medium ">
-
-                        Read more
-                        <svg
-                          className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 14 10"
+                      <div>
+                        <div
+                          class=" bg-gray-200 rounded-full "
+                          style={{ width: "100%" }}
                         >
-                          <path
-                            stroke="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M1 5h12m0 0L9 1m4 4L9 9"
-                          />
-                        </svg>
-                      </Link>
-                    </button>
+                          <div
+                            class="bg-violet-600 text-xs font-medium text-violet-100 text-center p-0.5 leading-none rounded-full"
+                            style={{ width: `${progress}%` }}
+                          >
+                            {" "}
+                            {progress}%
+                          </div>
+                        </div>
+                      </div>
+
+                      <button className="  border border-violet-500  hover:bg-violet-100 h-8 my-3 rounded text-violet-500 font-semibold">
+                        <Link
+                          to={`/student/courses/${course.id}`}
+                          state={{ course }}
+                          className="inline-flex items-center px-3 py-2 text-sm text-center font-medium "
+                        >
+                          Read more
+                          <svg
+                            className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 14 10"
+                          >
+                            <path
+                              stroke="currentColor"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M1 5h12m0 0L9 1m4 4L9 9"
+                            />
+                          </svg>
+                        </Link>
+                      </button>
+                    </div>
                   </div>
-                </div>
+                );
               })}
             </div>
           </div>
