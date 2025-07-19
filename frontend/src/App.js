13a4a8f858f472e00ed465349583c234381455ab
Namnika <janbandhunamnika@gmail.com>
2025-07-19 18:31:24 +0530

    handle routers as per roles for admins and students

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,89 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import AdminDashboard from "./components/Admin/AdminDashboard";
 import "./index.css";
 import Signup from "./components/Auth/Signup";
 import StudentDashboard from "./components/Student/StudentDashboard";
 import CourseView from "./components/Student/CourseView";
+import { useAuth, AuthProvider } from "./context/AuthContext";
+import { useEffect } from "react";
+import { BeatLoader } from "react-spinners";
 
-// const PrivateRoute = ({ children, role }) => {
-//   const token = localStorage.getItem('token');
-//   const user = JSON.parse(localStorage.getItem('user'));
+export const ProtectedRoute = ({ children, role }) => {
+  const { user, authLoading } = useAuth();
 
-//   if (!token || !user) return <Navigate to="/login" />;
+  const navigate = useNavigate()
 
-//   // Optionally check role
-//   if (role && user.role !== role) {
-//     return <Navigate to="/unauthorized" />;
-//   }
+  useEffect(() => {
+    if (!authLoading) {
+      if (!user) { navigate("/login/users") }
+      else if (role) {
+        const allowedRoles = Array.isArray(role) ? role : [role];
+        if (!allowedRoles.includes(user.role)) {
+          navigate("/login/users");
+        }
+      }
+    }
 
-//   return children;
-// };
 
-// export default PrivateRoute;
-// eslint-disable-next-line no-lone-blocks
-{
-  /* */
+
+  }, [user, navigate, role, authLoading])
+
+  if (authLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <BeatLoader color="#36d7b7" />
+      </div>
+    );
+  }
+
+
+  return user ? children : null;
+}
+
+export const SuperAdminRoute = ({ children }) => {
+  const { user } = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user) { navigate("/login/users") }
+    if (user == null) {<p>No Access to unauthorized user!</p>}
+  }, [user, navigate])
+
+  return children
 }
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login/users" element={<Login />} />
-        <Route path="/student/dashboard" element={<StudentDashboard />} />
-        <Route path="/student/courses/:id" element={<CourseView />}/>
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        
-        {/* <Route
-          path="/admin/dashboard"
-          element={
-            <PrivateRoute role="admin">
+      <AuthProvider>
+
+        <Routes>
+          {/* <Route path="/login/users" element={<Login />} /> */}
+          <Route path="/login/users" element={<Login />} />
+          <Route path="/student/dashboard" element={
+            <ProtectedRoute role={["student", "admin", "owner"]}>
+              <StudentDashboard />
+            </ProtectedRoute>
+          } />
+          <Route path="/student/courses/:id" element={
+            <ProtectedRoute role={["student", "admin", "owner"]}>
+              <CourseView />
+            </ProtectedRoute>
+          } />
+          <Route path="/admin/dashboard" element={
+            <ProtectedRoute role={["admin", "owner"]}>
               <AdminDashboard />
-              <SignUp/> // superadmin 
-            </PrivateRoute>
-          }
-        /> */}
-      </Routes>
+            </ProtectedRoute>
+          } />
+
+          <Route path="/users" element={
+            <SuperAdminRoute role="owner">
+              <Signup />
+            </SuperAdminRoute>
+          } />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
