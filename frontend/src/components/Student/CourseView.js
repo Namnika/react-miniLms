024a8e21875cfd359d4ddd3ac17aa023a4896b7b
Namnika <janbandhunamnika@gmail.com>
2025-07-19 14:41:23 +0530

    remove unnecessary imports

--- frontend/src/components/Student/CourseView.js
+++ frontend/src/components/Student/CourseView.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
